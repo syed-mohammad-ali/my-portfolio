8af7338fadc18df898d7e225745e027951994755
syed-mohammad-ali <sm9009995710@gmail.com>
2025-06-26 12:51:58 +0530

    update: latest changes before deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/", // ✅ Important for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
